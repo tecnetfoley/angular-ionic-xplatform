018ab9a5288213d3558d26c82fad0d81ffe49587
Brian Foley <foley@tecnet.co.jp>
2019-01-07 11:06:55 +0900

    updated package and webpack

--- electron-webpack.config.js
+++ electron-webpack.config.js
@@ -5,16 +5,16 @@ const platform = process.env.PLATFORM || process.platform;
 module.exports = {
     target: 'electron-renderer',
     externals: {
-      "better-sqlite3": "commonjs better-sqlite3",
-      sqlite3: "commonjs sqlite3"
+      // "better-sqlite3": "commonjs better-sqlite3", Example better-sqlite3
+      // sqlite3: "commonjs sqlite3" Example sqlite3 
     },    
-    module: {
-      rules: [
-        {
-          test: /\.node$/,
-          use: 'node-loader'
-        }
-      ]
-    }
+    // module: {
+    //   rules: [
+    //     {
+    //       test: /\.node$/,
+    //       use: 'node-loader'
+    //     }
+    //   ]
+    // }
     
   };
